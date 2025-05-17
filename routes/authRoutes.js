0919a7dd548571b9565fa4510aa8e2941648a23e
Sara <sara.benyahya2014@gmail.com>
2025-05-17 23:28:02 +0100

    modif

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const AuthController = require("../controllers/authController");
-
-router.post("/login", AuthController.loginUser);
-router.post("/logout", AuthController.logoutUser);
-router.post("/register", AuthController.registerUser);
-
-module.exports = router;
